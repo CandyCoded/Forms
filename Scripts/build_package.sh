d0546896b1bd925350dd53bbec9422e5c486cf89
Scott Doxey <neogeek0101@gmail.com>
2021-01-09 13:26:09 -0500

    Switch to using find-unity to build package.

--- Scripts/build_package.sh
+++ Scripts/build_package.sh
@@ -4,7 +4,7 @@ mkdir -p Build
 
 BUILD_TAG=$(cat Assets/Plugins/CandyCoded.Forms/Version.txt)
 
-LATEST_UNITY_VERSION=$(find /Applications/Unity -name Unity.app | sort -r | head -1)
+LATEST_UNITY_VERSION=$(find-unity)
 
 echo "Building with ${LATEST_UNITY_VERSION}"
 
